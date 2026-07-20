1aa3d784a656b8d63447112f996440751ccd491d
AhmedMedhat <75226176+AhmedMedhat77@users.noreply.github.com>
2026-07-20 16:51:09 +0300

    refactor: reorder and organize imports in app.tsx for better maintainability

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,16 +1,16 @@
-import LoginPage from '@/features/auth/pages/login/login.page';
-import EmailPage from '@/features/auth/pages/registration/email.page';
-import VerifyOtpPage from '@/features/auth/pages/registration/verify-otp.page';
-import UserInfoPage from '@/features/auth/pages/registration/user-info.page';
-import PasswordPage from '@/features/auth/pages/registration/password.page';
-import ForgotPasswordPage from '@/features/auth/pages/forgot-password/forgot-password.page';
-import ResetLinkSentPage from '@/features/auth/pages/forgot-password/reset-link-sent.page';
-import ResetPasswordPage from '@/features/auth/pages/forgot-password/reset-password.page';
-import { BrowserRouter, Route, Routes } from 'react-router';
 import RootLayout from '@/app/layouts/root-layout';
 import DashboardHomePage from '@/app/pages/dashboard-home-page';
 import { AppProviders } from '@/app/providers';
 import { ROUTES } from '@/app/routes';
+import ForgotPasswordPage from '@/features/auth/pages/forgot-password/forgot-password.page';
+import ResetLinkSentPage from '@/features/auth/pages/forgot-password/reset-link-sent.page';
+import ResetPasswordPage from '@/features/auth/pages/forgot-password/reset-password.page';
+import LoginPage from '@/features/auth/pages/login/login.page';
+import EmailPage from '@/features/auth/pages/registration/email.page';
+import PasswordPage from '@/features/auth/pages/registration/password.page';
+import UserInfoPage from '@/features/auth/pages/registration/user-info.page';
+import VerifyOtpPage from '@/features/auth/pages/registration/verify-otp.page';
+import { BrowserRouter, Route, Routes } from 'react-router';
 
 export function App() {
   return (
